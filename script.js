83ed3fec367b9b7dc696702be0c9938e0f832350
theakarshit <85669530+theakarshit@users.noreply.github.com>
2024-10-17 19:47:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('login-form').addEventListener('submit', function (event) {
     event.preventDefault();
     const password = document.getElementById('password').value;
-    const correctPassword = '0923'; // Set your password here
+    const correctPassword = '0923'; // Set the password here
     if (password === correctPassword) {
-        document.getElementById('protected-content').style.display = 'block';
-        document.getElementById('login-form').style.display = 'none';
+        // Redirect the user to a new page (for now you can use "newpage.html")
+        window.location.href = 'newpage.html'; // You can replace 'newpage.html' with any page name
     } else {
         alert('Incorrect password');
     }
