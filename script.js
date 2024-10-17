8a03b49e1d18c138fb33221f61001b849b6c97af
theakarshit <85669530+theakarshit@users.noreply.github.com>
2024-10-17 19:44:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('login-form').addEventListener('submit', function (event) {
     event.preventDefault();
     const password = document.getElementById('password').value;
-    const correctPassword = 'your_password'; // Set your password here
+    const correctPassword = '0923'; // Set your password here
     if (password === correctPassword) {
         document.getElementById('protected-content').style.display = 'block';
         document.getElementById('login-form').style.display = 'none';
